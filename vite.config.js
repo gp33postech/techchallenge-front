58a428356a4a89c39552e69d250151866fc8a245
HADSADRIANO <adriano_araujo6@yahoo.com.br>
2025-05-10 16:20:05 -0300

    Corrige base path para deploy no Render

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <- ESSENCIAL para o Render servir os arquivos corretamente
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory for the build
-  }
+    outDir: 'dist',
+  },
 })
